994127b449d44d071b7ac2cd6e3aa9e0286aa40b
Yehor Sergeenko <yehor.sergeenko@gmail.com>
2013-03-13 02:30:41 +0300

    Update dope.js

--- dope.js
+++ dope.js
@@ -1,5 +1,6 @@
 (function() {'use strict';
   var dope = function(uri, callback, timeout) {
+    var queue = null;
     typeof (uri) === 'string' ? uri = [uri] : uri = uri;
     for (var i in uri) {
       var el = null;
@@ -24,17 +25,23 @@
           if (!this.fired || this.readyState === 'loaded' || this.readyState === 'complete') {
             this.fired = true;
             this.onreadystatechange = this.onload = null;
-            setTimeout(function() {
-              callback();
-            }, timeout);
+            queue += 1;
           }
         }
       }
 
       document.getElementsByTagName('head')[0].appendChild(el);
     }
-  }
 
+    if (callback && typeof (callback) === 'function') {
+      if (queue == uri.lenght) {
+        setTimeout(function() {
+          callback();
+        }, timeout);
+      }
+    }
+
+  }
   if (/msie [1-8]\./.test(navigator.userAgent.toLowerCase())) {
     dope(['assets/js/polyfill.js', 'assets/js/augment.js'], function(e) {
       console.log('WOOP!');
