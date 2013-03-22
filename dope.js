ba5903881de2c87d46dde095b2b5dcbb08936a2b
Yehor Sergeenko <yehor.sergeenko@gmail.com>
2013-03-22 14:13:31 +0300

    Update dope.js

--- dope.js
+++ dope.js
@@ -70,7 +70,14 @@ if (/msie [1-8]\./.test(navigator.userAgent.toLowerCase())) {
 if (document.location.href == '//somewhe.re/urn/') {
   dope('assets/js/common.js', function(e) {
     console.log('WOOP!');
-}, 0, 5000);
+  }, 1000, 5000);
+}
+
+if (/^.*(urn$)/i.test(window.location.href)) {
+  dope('assets/js/common.js', function(e) {
+    console.log('WOOP!');
+  }, 1000, 5000);
+}
 
 dope('assets/css/common.js', function(e) {
   console.log('WOOP!');
