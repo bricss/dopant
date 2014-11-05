0b7f9ac6d2824b39429adf407d1f519e7e673b28
yehor.sergeenko <yehor.sergeenko@gmail.com>
2014-11-06 00:24:25 +0300

    Rebuild

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
+var pkg = require('./package.json');
+
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
 
 gulp.task('default', function() {
-  return gulp.src('./src/*.js').pipe(gulp.dest('./dist')).pipe(rename(pkg.name + '.min.js')).pipe(uglify({
+  return gulp.src('./src/*.js').pipe(gulp.dest('./dist')).pipe(rename({
+    suffix : '.min'
+  })).pipe(uglify({
     compress : true,
     preserveComments : 'some'
   })).pipe(gulp.dest('./dist'));
